1b69bc3f1ba2a946737ff67d945fe5486c9f1e4e
Philip Hallstrom <philip@pjkh.com>
2011-08-01 15:19:55 -0700

    Add :format option for date/time/string/numeric values. Fix html_safe issues

--- lib/simple_show/base.rb
+++ lib/simple_show/base.rb
@@ -28,8 +28,16 @@ module SimpleShow
       else
         value = options[:value] || @record.send(attr)
       end
+      if options[:format].present?
+        if [:datetime, :timestamp, :time, :date].include?(@record.class.columns_hash[attr.to_s].type)
+          value = value.send(options[:format].is_a?(Symbol) ? :to_s : :strftime, options[:format])
+        else
+          value = options[:format] % value
+        end
+      end
+
       @binding.content_tag(SimpleShow.value_tag, :class => SimpleShow.value_class) do
-        [SimpleShow.value_prefix, value, SimpleShow.value_suffix].compact.join
+        [SimpleShow.value_prefix, value, SimpleShow.value_suffix].compact.join.html_safe
       end
     end
   end
--- test/test_simple_show_value.rb
+++ test/test_simple_show_value.rb
@@ -2,19 +2,45 @@ require 'test_helper'
 
 class SimpleShowValueTest < SimpleShowTestCase
 
-  test 'values and value overrides' do
-    doc = Nokogiri::HTML.fragment(
+  def setup
+    super
+    @doc = Nokogiri::HTML.fragment(
       simple_show_for @philip do |s| 
         o  = ActiveSupport::SafeBuffer.new
         o += s.show :name
         o += s.show(:email) {|o| o.email.upcase }
         o += s.show :phone, :value => '867-5309'
+        o += s.show :born_on, :format => '%B %d'
+        o += s.show :born_on, :format => :mmddyy
+        o += s.show(:html) {|o| '<b>html</b>' }
+        o += s.show :handicap, :format => '%.3f'
+        o += s.show :name, :format => '%20s'
         o
       end
     )
-    assert_equal 'Philip Hallstrom', doc.css('span.value')[0].text
-    assert_equal 'PHILIP@PJKH.COM', doc.css('span.value')[1].text
-    assert_equal '867-5309', doc.css('span.value')[2].text
+  end
+
+  test 'values and value overrides' do
+    assert_equal 'Philip Hallstrom', @doc.css('span.value')[0].text
+    assert_equal 'PHILIP@PJKH.COM', @doc.css('span.value')[1].text
+    assert_equal '867-5309', @doc.css('span.value')[2].text
+  end
+
+  test 'date/time with formatting' do
+    assert_equal 'May 24', @doc.css('span.value')[3].text
+    assert_equal '05/24/74', @doc.css('span.value')[4].text
+  end
+
+  test 'values containing html' do
+    assert_equal '<b>html</b>', @doc.css('span.value')[5].inner_html
+  end
+
+  test 'numbers with formatting' do
+    assert_equal '6.500', @doc.css('span.value')[6].text
+  end
+
+  test 'strings with formatting' do
+    assert_equal '    Philip Hallstrom', @doc.css('span.value')[7].text
   end
 
 end
