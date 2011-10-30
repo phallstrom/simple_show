627641715d767fabcb721daca98499895a172f4b
Dan Milliron <danm@alum.mit.edu>
2011-10-30 08:26:14 -0700

    Include a show field's type as a CSS class

--- lib/simple_show/base.rb
+++ lib/simple_show/base.rb
@@ -63,7 +63,8 @@ module SimpleShow
         end
       end
 
-      @binding.content_tag(SimpleShow.value_tag, :class => SimpleShow.value_class) do
+      field_type = (@record.class.columns_hash[attr.to_s].type rescue nil)
+      @binding.content_tag(SimpleShow.value_tag, :class => [SimpleShow.value_class, field_type].compact) do
         [SimpleShow.value_prefix, value, SimpleShow.value_suffix].compact.join.html_safe
       end
     end
